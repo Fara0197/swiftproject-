0703e2b4846fd5a998dca54416e7cdeb7844600d
Nasra <fara0197@algonquinlive.com>
2022-11-30 15:02:48 -0500

    Initial Commit

---
+++ final-project/final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_projectApp.swift
+//  final-project
+//
+//  Created by Nasra Farah on 2022-11-30.
+//
+
+import SwiftUI
+
+@main
+struct final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
